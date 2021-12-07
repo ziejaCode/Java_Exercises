ebc659e02921d77ac96e3fb75413136db923a7b2
ziejaCode <49070294+ziejaCode@users.noreply.github.com>
2021-12-07 02:11:36 +0000

    Commit 3

---
+++ Core_Exercises/src/Chapter2/Exe1/Testing1.java
@@ -0,0 +1,10 @@
+package Chapter2.Exe1;
+
+public class Testing1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Core_Exercises/src/Chapter2/Exe2/Testing2.java
@@ -0,0 +1,5 @@
+package Chapter2.Exe2;
+
+public class Testing2 {
+
+}
