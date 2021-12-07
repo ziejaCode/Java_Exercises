86b19f1c27c8a01f02aba2dfbad00045044261de
ziejaCode <49070294+ziejaCode@users.noreply.github.com>
2021-12-07 01:28:46 +0000

    Commit2

---
+++ Core_Exercises/src/Chapter1/Exe1/Testing1.java
@@ -0,0 +1,9 @@
+package Chapter1.Exe1;
+
+public class Testing1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello form testing1");
+	}
+
+}
---
+++ Core_Exercises/src/Chapter1/Exe1/Testing2.java
@@ -0,0 +1,9 @@
+package Chapter1.Exe1;
+
+public class Testing2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello form testing2");
+	}
+
+}
